d27c8e8333d05e1b8691a2f16a85e184578461d9
®️DonDada_1703™️ <109308073+bylickilabs@users.noreply.github.com>
2023-01-02 22:32:51 +0100

    Update English_Decrypt.php

--- English_Decrypt.php
+++ English_Decrypt.php
@@ -2,3 +2,4 @@
 <br>
 &nbsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;<font color="red">¦¦¦¦¦¦+¦¦¦+¦¦¦¦¦+¦¦+¦¦¦¦¦¦¦+¦¦¦¦¦¦+¦¦¦+¦¦¦¦+¦¦+¦¦+¦¦¦¦¦¦¦¦¦¦¦+¦¦¦¦¦¦¦+¦¦¦¦¦¦¦¦+</font><br />
 &nbsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;<font color="red">¦¦+--¦¦++¦¦+¦¦¦++¦¦¦¦¦¦¦¦¦¦¦¦¦+--¦¦+¦¦¦¦¦¦++¦¦¦¦¦¦¦¦¦¦¦¦¦+--¦¦+¦¦+--¦¦+¦¦+----+</font><br />
+&nbsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;<font color="red">¦¦¦¦¦¦-+¦+¦¦¦¦++¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦+-+¦¦¦¦¦-+¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦-++¦¦¦¦¦+¦</font><br />
