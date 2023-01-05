8ce35b219f5e59f1f4857a5128444d23cb53266b
®️Thorsten Bylicki™️ <109308073+bylickilabs@users.noreply.github.com>
2023-01-05 09:06:59 +0100

    Update German_Decrypt.php

--- German_Decrypt.php
+++ German_Decrypt.php
@@ -86,4 +86,4 @@ $content .= '
 <br>&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;<input name="anzahl" value="'.$_POST['anzahl'].'" />
 <input name="submit" type="submit" value="Generieren" />
 <br>
-
+<br>
