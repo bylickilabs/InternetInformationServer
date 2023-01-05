3c63a6520caa05478f8fa0e53b8fdbb3e42ec5e3
®️Thorsten Bylicki™️ <109308073+bylickilabs@users.noreply.github.com>
2023-01-06 00:25:50 +0100

    Update German_Decrypt.php

--- German_Decrypt.php
+++ German_Decrypt.php
@@ -143,4 +143,5 @@ $content .= '
 <summary>Kontakt</summary>
 <font color="#008CFF">Thorsten Bylicki</font><br />
 <font color="#008CFF">www.bylickilabs.de</font><br />
+<font color="#008CFF">Email: 109308073+bylickilabs@users.noreply.github.com</font><br />
 
