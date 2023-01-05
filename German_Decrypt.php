cbfcb7428f3e2053234960642ea0c44c4c66e79e
®️Thorsten Bylicki™️ <109308073+bylickilabs@users.noreply.github.com>
2023-01-06 00:47:40 +0100

    Update German_Decrypt.php

--- German_Decrypt.php
+++ German_Decrypt.php
@@ -169,4 +169,5 @@ $content .= '
 <a href="https://developer.android.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg" alt="android" width="40" height="40"/> </a> <a href="https://dotnet.microsoft.com/apps/xamarin" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/xamarin.svg" alt="xamarin" width="40" height="40"/> </a> <a href="https://reactnative.dev/" target="_blank" rel="noreferrer"> <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40"/> </a>
 </details></li>
 <li>
+<details>
 
