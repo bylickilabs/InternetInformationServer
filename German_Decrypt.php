45e438b61ecc49f23947ca530211dc3457b94135
®️Thorsten Bylicki™️ <109308073+bylickilabs@users.noreply.github.com>
2023-01-05 08:55:59 +0100

    Update German_Decrypt.php

--- German_Decrypt.php
+++ German_Decrypt.php
@@ -67,4 +67,5 @@ if (isset($_POST['anzahl'])){
     if ($_POST['anzahl'] >= 46){
         $_POST['anzahl'] = 46;
     }elseif($_POST['anzahl'] == '' || $_POST['anzahl'] <= 0){
+        $_POST['anzahl'] = 46;
 
