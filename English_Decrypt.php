46f947ef8b20765bf8214f690cf313b72ac9b715
®️Thorsten Bylicki™️ <109308073+bylickilabs@users.noreply.github.com>
2023-01-07 20:08:59 +0100

    Update English_Decrypt.php

--- English_Decrypt.php
+++ English_Decrypt.php
@@ -67,5 +67,6 @@ function generateSymbols($num = 46){
 if (isset($_POST['quantity'])){
     $_POST['quantity'] = preg_replace('/([^0-9]*)/', '', $_POST['quantity']);
     if ($_POST['quantity'] >= 46){
+        $_POST['quantity'] = 46;
 
 ```
