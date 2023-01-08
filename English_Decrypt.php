a5ea3f680a9f779d7d28472b1af614251e241ae5
®️Thorsten Bylicki™️ <109308073+bylickilabs@users.noreply.github.com>
2023-01-08 12:09:18 +0100

    Update English_Decrypt.php

--- English_Decrypt.php
+++ English_Decrypt.php
@@ -116,5 +116,12 @@ $content .= '
 </details></li>
 <li>
 <details>
+<summary>TikTok</summary>
+<font color="#008CFF">https://www.tiktok.com/@thobyl_82</font><br />
+</details></li>
+<li>
+<details>
+<summary>soundcloud</summary>
+<font color="#008CFF">https://soundcloud.com/don-dada-1703</font><br />
 
 ```
